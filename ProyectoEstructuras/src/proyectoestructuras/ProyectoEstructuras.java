ec94f36b2f02e3daa5a44f51e18c6fcba0bf53d4
indir <indirarguz04mail.com>
2023-10-07 14:24:46 -0600

    carga inicial

--- ProyectoEstructuras/src/proyectoestructuras/ProyectoEstructuras.java
+++ ProyectoEstructuras/src/proyectoestructuras/ProyectoEstructuras.java
@@ -4,6 +4,8 @@
  */
 package proyectoestructuras;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author indir
@@ -15,8 +17,35 @@ public class ProyectoEstructuras {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        //Probando
-        System.out.println("Probando");
+         
+       int opcion;
+        do {
+            opcion = Integer.parseInt(JOptionPane.showInputDialog("""
+                    Seleccione una opción:
+                    1.Iniciar sesion Master
+                    2.Iniciar sesion empleado
+                    3.Informacion
+                    0.Salir del sistema
+                    """)
+            );
+               
+            switch (opcion) {
+                case 1 -> { 
+                    JOptionPane.showMessageDialog(null, "Iniciar sesion master");
+                
+                }case 2 ->{ 
+                    JOptionPane.showMessageDialog(null, "Iniciar sesion empleado");
+
+                }case 3 ->{ 
+                    JOptionPane.showMessageDialog(null, "Se muestra la informacion");
+                }case 0 ->{ 
+                    JOptionPane.showMessageDialog(null, "Adios");
+                    break;
+                }default ->
+                    JOptionPane.showMessageDialog(null, "Opción inválida. "
+                            + "Por favor, seleccione nuevamente.");}
+        }while (opcion != 0);   
+    }
     }
     
-}
+
