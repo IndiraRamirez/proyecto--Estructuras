ec3362e7b71a0e27e3c83e86f0abdb0fe27952b5
indir <indirarguz04mail.com>
2023-10-07 19:15:48 -0600

    carga inicial

--- ProyectoEstructuras/src/proyectoestructuras/Cliente.java
+++ ProyectoEstructuras/src/proyectoestructuras/Cliente.java
@@ -12,17 +12,21 @@ import javax.swing.JOptionPane;
  */
 public class Cliente {
     
-     private String nombre,correo;
-     private int  edad,id, numeroTel;
+     public String nombre,correo;
+     public int  edad,id, numeroTel;
 
-    public Cliente(String nombre, int edad, int id, int numeroTel) {
+    public Cliente(String nombre, int edad, int id, int numeroTel, String correo) {
         this.nombre = nombre;
+        this.correo = correo;
         this.edad = edad;
         this.id = id;
         this.numeroTel = numeroTel;
-        correo="";
     }
-     public String getNombre() {return nombre;}
+
+    Cliente() {
+    }
+
+    public String getNombre() {return nombre;}
     public void setNombre(String nombre) {this.nombre = nombre;}
 
     public int getId() {return id;}
@@ -30,6 +34,13 @@ public class Cliente {
 
     public int getEdad() {return edad;}
     public void setEdad(int edad) {this.edad = edad;}
+
+    public int getNumeroTel() {
+        return numeroTel;}
+    public void setNumeroTel(int numeroTel) {
+        this.numeroTel = numeroTel;}
+    
+    
 
    public String getCorreo(){
         if(correo.equals("")){
@@ -40,28 +51,53 @@ public class Cliente {
                 correo.substring(correo.indexOf("@"));
     }
     
-    public void setCorreo(String pCorreo){
-        if(pCorreo.contains("@")){
-          
-            if(pCorreo.substring(pCorreo.indexOf("@"))
-                    .equals("@gmail.com")){
-                correo=pCorreo;
-            }else{
-                System.out.println("Error: correo no valido"+
-                        " debe ser una cuenta gmail");
-            }
-        }else{
-            System.out.println("Error: correo no valido"+
-                " debe contener @");
-        }
+    public void setCorreo(String correo){{this.correo = correo;}  
     }
+    public String correoDado(){
+        String correoDado;
+        do{   
+        correoDado=JOptionPane.showInputDialog("Ingrese su correo: ");
+            if(correoDado.contains("@")){
+                if(correoDado.substring(correoDado.indexOf("@"))
+                        .equals("@gmail.com")){
+                    this.correo=correoDado;
+                if(correoDado.substring(correoDado.indexOf("@"))
+                        .equals("@hotmail.com")){
+                        this.correo=correoDado;
+                        }
+                }else{
+                    JOptionPane.showInputDialog("Error: correo no valido"+
+                            " debe ser una cuenta valida");
+                }
+            }else{
+               JOptionPane.showInputDialog("Error: correo no valido"+
+                    " debe contener @");
+            }
+        }while(this.correo==null ||this.correo.equals(""));
+        return correoDado;
+     }
 
-     public  Cliente CrearTiquete() {
+     public  Cliente CrearCliente() {
             String nombre = JOptionPane.showInputDialog("Ingrese el nombre : ");
             int edad = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la edad  :"));
             int id = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el numero de identificacion :"));
             int numeroTel = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el numero telefono:"));
-            
-            return new Cliente(nombre, edad, id,numeroTel);   
-    } 
+            String correo=correoDado();
+              return new Cliente(nombre,edad,id,numeroTel,correo);
+            } 
+
+     @Override
+    public String toString() { 
+        return """
+                Cliente
+                Nombre=""" + this.nombre +"\n"+
+                "Edad=" + this.edad +"\n"+
+                "Id=" + this.id +"\n"+
+                "Numero telefonico="+this.numeroTel+"\n"+
+                "Correo electronico="+getCorreo()+"\n";
+    }
+
+
+   
+     
 }
