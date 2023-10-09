5103c919d9dc9b9ef6cea327222f49bb70411d16
indir <indirarguz04mail.com>
2023-10-08 22:22:29 -0600

    nueva clase

---
+++ ProyectoEstructuras/src/proyectoestructuras/Customer.java
@@ -0,0 +1,78 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyectoestructuras;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author indir
+ */
+public class Customer {
+       public String nombre,correo;
+     public int  edad,id, numeroTel;
+
+    public  Customer (String nombre, int edad, int id, int numeroTel, String correo) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.id = id;
+        this.numeroTel = numeroTel;
+        this.correo = correo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getNumeroTel() {
+        return numeroTel;
+    }
+
+    public void setNumeroTel(int numeroTel) {
+        this.numeroTel = numeroTel;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+   public String getCorreo(){
+        if(correo.equals("")){
+            return "Error: correo no ingresado";
+        }
+        
+        return correo.substring(0, 3)+"....."+
+                correo.substring(correo.indexOf("@"));
+    }
+    @Override
+    public String toString() { 
+        return """
+                Cliente
+                Nombre=""" + nombre +"\n"+
+                "Edad=" + edad +"\n"+
+                "Id=" + id +"\n"+
+                "Numero telefonico="+numeroTel+"\n"+
+                "Correo electronico="+getCorreo()+"\n";
+    }
+}
