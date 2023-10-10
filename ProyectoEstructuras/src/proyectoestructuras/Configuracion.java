7802fef406b8d6866898c9e4db22431d59b02fcb
indir <indirarguz04mail.com>
2023-10-10 14:53:01 -0600

    Cambios

--- ProyectoEstructuras/src/proyectoestructuras/Configuracion.java
+++ ProyectoEstructuras/src/proyectoestructuras/Configuracion.java
@@ -11,12 +11,25 @@ package proyectoestructuras;
 public class Configuracion {
     public static String nombre = "AzzaCar";
     public static String telefono = "8888-8888";
-    public static String direccion = "Cartago, Cartago, Occidental";
+    public static String direccion = " Cartago, Occidental";
 
-    public static void mostrarInformacionEmpresa() {
-        System.out.println("Nombre: " + nombre);
-        System.out.println("Teléfono: " + telefono);
-        System.out.println("Dirección: " + direccion);
+    
+    public static String info(){
+        return nombre+"\n"+telefono+"\n"+direccion;
+    }
+    
+    
+    
+        //System.out.println("Nombre: " + nombre);
+        //System.out.println("Teléfono: " + telefono);
+        //System.out.println("Dirección: " + direccion);
+    
+    public String toString() {
+        return """
+               Empresa
+               """ +nombre+"\n"+
+                "Nos encontramos en "+direccion+"\n"+
+                "Para más informacion contactanos "+"numero de telefono"+telefono+"\n";
     }
 }
 
