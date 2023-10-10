1d3016216001bb20f7f1a53ed3bd90f16cc3677b
Pernudiii <jeffersonpernudisolano@gmail.com>
2023-10-10 14:09:19 -0600

    nueva class

---
+++ ProyectoEstructuras/src/proyectoestructuras/Configuracion.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyectoestructuras;
+
+/**
+ *
+ * @author Jeffe
+ */
+public class Configuracion {
+    public static String nombre = "AzzaCar";
+    public static String telefono = "8888-8888";
+    public static String direccion = "Cartago, Cartago, Occidental";
+
+    public static void mostrarInformacionEmpresa() {
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Teléfono: " + telefono);
+        System.out.println("Dirección: " + direccion);
+    }
+}
+
+//ConfiguracionEmpresa.mostrarInformacionEmpresa();
+   
+
