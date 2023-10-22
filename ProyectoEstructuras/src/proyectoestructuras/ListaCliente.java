c1064b2c8dd0432b396831410722da1167e7a789
indir <indirarguz04mail.com>
2023-10-21 21:39:48 -0600

    Cambios

---
+++ ProyectoEstructuras/src/proyectoestructuras/ListaCliente.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyectoestructuras;
+
+/**
+ *
+ * @author indir
+ */
+public class ListaCliente {
+     private NodoCliente cabeza;
+
+    public ListaCliente() {
+        cabeza = null;
+    }
+
+    public NodoCliente getCabeza() {
+        return cabeza;
+    }
+
+    // Agregar persona ordenada por id
+    public void agregarPersonaOrdenado(Customer persona) {
+        NodoCliente nuevoNodo = new NodoCliente(persona);
+        if (cabeza == null || cabeza.getPersona().getId() > persona.getId()) {
+            nuevoNodo.setSiguiente(cabeza);
+            cabeza = nuevoNodo;
+        } else {
+            NodoCliente aux = cabeza;
+            while (aux.getSiguiente() != null && aux.getSiguiente().getPersona().getId() < persona.getId()) {
+                aux = aux.getSiguiente();
+            }
+            nuevoNodo.setSiguiente(aux.getSiguiente());
+            aux.setSiguiente(nuevoNodo);
+        }
+    }
+
+    // Método para mostrar personas
+    public void mostrarPersonas() {
+        NodoCliente aux = cabeza;
+        while (aux != null) {
+            System.out.println(aux.getPersona());
+            aux = aux.getSiguiente();
+        }
+    }
+}
+
+
