f61b20341341a318b487b18ddc68b39114faf387
Jeffry Canales Herrera <123007989+JeffryCH@users.noreply.github.com>
2023-10-25 20:25:42 -0600

    Create NodoVehiculo.java
    Nodo aun pendiente

---
+++ ProyectoEstructuras/src/proyectoestructuras/NodoVehiculo.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyectoestructuras;
+
+/**
+ *
+ * @author JeffryCH
+ */
+public class NodoVehiculo {
+    private Vehiculo vehiculo;
+    private NodoVehiculo siguiente;
+
+    public NodoVehiculo(Vehiculo vehiculo) {
+        this.vehiculo = vehiculo;
+        this.siguiente = null;
+    }
+
+    public Vehiculo getVehiculo() {
+        return vehiculo;
+    }
+
+    public NodoVehiculo getSiguiente() {
+        return siguiente;
+    }
+
+    public void setSiguiente(NodoVehiculo siguiente) {
+        this.siguiente = siguiente;
+    }
+}
