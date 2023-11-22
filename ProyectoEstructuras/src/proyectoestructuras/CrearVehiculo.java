d7d395ab4acadff7aea9e81fa9123a4717dce614
Jeffry Canales Herrera <123007989+JeffryCH@users.noreply.github.com>
2023-11-22 15:37:34 -0600

    Update CrearVehiculo.java
    Se actualiza el codigo

--- ProyectoEstructuras/src/proyectoestructuras/CrearVehiculo.java
+++ ProyectoEstructuras/src/proyectoestructuras/CrearVehiculo.java
@@ -1,33 +1,104 @@
 package proyectoestructuras;
 
+import javax.swing.JOptionPane;
+
+
 /**
  *
  * @author JeffryCH
  */
-import javax.swing.JOptionPane;
 
 
 public class CrearVehiculo {
 
     public static Vehiculo crearNuevoVehiculo() {
-        // Solicitar la información del vehículo al usuario
-        String color = JOptionPane.showInputDialog("Ingrese el color del vehículo:");
-        int anio = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el año del vehículo:"));
-        int cilindraje = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el cilindraje del vehículo:"));
-        String marca = JOptionPane.showInputDialog("Ingrese la marca del vehículo:");
-        String modelo = JOptionPane.showInputDialog("Ingrese el modelo del vehículo:");
-        double kilometraje = Double.parseDouble(JOptionPane.showInputDialog("Ingrese el kilometraje del vehículo:"));
-        Vehiculo.TipoVehiculo tipo = Vehiculo.TipoVehiculo.valueOf(JOptionPane.showInputDialog("Ingrese el tipo de vehículo (SUV, SEDAN, HATCHBACK):").toUpperCase());
-        String caracteristicas = JOptionPane.showInputDialog("Ingrese las características del vehículo:");
+        String marca = JOptionPane.showInputDialog("Escribe la marca del vehículo");
+        String modelo = JOptionPane.showInputDialog("Escribe el modelo del vehículo");
+        String color = JOptionPane.showInputDialog("Escribe el color del vehículo");
+        int anio = Integer.parseInt(JOptionPane.showInputDialog("Escribe el año del vehículo"));
 
-        // Crear un objeto Vehiculo con la información proporcionada
-        Vehiculo nuevoVehiculo = new Vehiculo(color, anio, cilindraje, marca, modelo, kilometraje, tipo, caracteristicas);
-        
+        // Solicitar cilindraje y kilometraje
+        int cilindraje = Integer.parseInt(JOptionPane.showInputDialog("Escribe el cilindraje del vehículo"));
+        double kilometraje = Double.parseDouble(JOptionPane.showInputDialog("Escribe el kilometraje del vehículo"));
+
+        Vehiculo.TipoVehiculo tipo = null;
+        Vehiculo.EstadoVehiculo estado = Vehiculo.EstadoVehiculo.DISPONIBLE; // Estado inicial
+        String caracteristicas = JOptionPane.showInputDialog("Escribe las características del vehículo");
+
+        // Asegúrate de que el usuario seleccione un tipo de vehículo
+        while (tipo == null) {
+            Object seleccion = JOptionPane.showInputDialog(
+                null,
+                "Selecciona el tipo de vehículo",
+                "Tipo de Vehículo",
+                JOptionPane.QUESTION_MESSAGE,
+                null,
+                Vehiculo.TipoVehiculo.values(),
+                Vehiculo.TipoVehiculo.values()[0]
+            );
+
+            if (seleccion != null) {
+                try {
+                    String seleccionStr = seleccion.toString();
+                    tipo = Vehiculo.TipoVehiculo.valueOf(seleccionStr.toUpperCase());
+                } catch (IllegalArgumentException e) {
+                    JOptionPane.showMessageDialog(null, "Por favor, selecciona un tipo de vehículo válido.");
+                }
+            } else {
+                JOptionPane.showMessageDialog(null, "No se seleccionó ningún tipo de vehículo.");
+                break; // Salir del bucle si el usuario cancela
+            }
+        }
+
+        // Crear el vehículo usando el constructor con todos los parámetros
+        Vehiculo nuevoVehiculo = new Vehiculo(marca, modelo, color, anio, cilindraje, kilometraje, tipo, caracteristicas, estado, "", "");
+
         return nuevoVehiculo;
     }
+}public class CrearVehiculo {
 
-    public static void main(String[] args) {
-        Vehiculo vehiculo = crearNuevoVehiculo();
-        JOptionPane.showMessageDialog(null, "Vehículo creado exitosamente:\n" + vehiculo.toString());
+    public static Vehiculo crearNuevoVehiculo() {
+        String marca = JOptionPane.showInputDialog("Escribe la marca del vehículo");
+        String modelo = JOptionPane.showInputDialog("Escribe el modelo del vehículo");
+        String color = JOptionPane.showInputDialog("Escribe el color del vehículo");
+        int anio = Integer.parseInt(JOptionPane.showInputDialog("Escribe el año del vehículo"));
+
+        // Solicitar cilindraje y kilometraje
+        int cilindraje = Integer.parseInt(JOptionPane.showInputDialog("Escribe el cilindraje del vehículo"));
+        double kilometraje = Double.parseDouble(JOptionPane.showInputDialog("Escribe el kilometraje del vehículo"));
+
+        Vehiculo.TipoVehiculo tipo = null;
+        Vehiculo.EstadoVehiculo estado = Vehiculo.EstadoVehiculo.DISPONIBLE; // Estado inicial
+        String caracteristicas = JOptionPane.showInputDialog("Escribe las características del vehículo");
+
+        // Asegúrate de que el usuario seleccione un tipo de vehículo
+        while (tipo == null) {
+            Object seleccion = JOptionPane.showInputDialog(
+                null,
+                "Selecciona el tipo de vehículo",
+                "Tipo de Vehículo",
+                JOptionPane.QUESTION_MESSAGE,
+                null,
+                Vehiculo.TipoVehiculo.values(),
+                Vehiculo.TipoVehiculo.values()[0]
+            );
+
+            if (seleccion != null) {
+                try {
+                    String seleccionStr = seleccion.toString();
+                    tipo = Vehiculo.TipoVehiculo.valueOf(seleccionStr.toUpperCase());
+                } catch (IllegalArgumentException e) {
+                    JOptionPane.showMessageDialog(null, "Por favor, selecciona un tipo de vehículo válido.");
+                }
+            } else {
+                JOptionPane.showMessageDialog(null, "No se seleccionó ningún tipo de vehículo.");
+                break; // Salir del bucle si el usuario cancela
+            }
+        }
+
+        // Crear el vehículo usando el constructor con todos los parámetros
+        Vehiculo nuevoVehiculo = new Vehiculo(marca, modelo, color, anio, cilindraje, kilometraje, tipo, caracteristicas, estado, "", "");
+
+        return nuevoVehiculo;
     }
 }
